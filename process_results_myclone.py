9fcbfc55696a7a72e1e4efa1c348042741a7c1df
Bianca Kolim <biancakolim@Biancas-MacBook-Pro-2.local>
2026-04-16 15:32:19 -0400

    edited process_results_myclone to add gene names to combined results

--- process_results_myclone.py
+++ process_results_myclone.py
@@ -80,3 +80,59 @@ myclone_results_combined_df.to_csv(output_path, sep="\t", index=False)
 print(myclone_results_combined_df.shape)
 print(myclone_results_combined_df.head())
 
+### ## ADD GENE NAMES TO MYCLONE RESULTS
+
+input_path = '/Users/biancakolim/Desktop/Academic/Spring2026/FCBB/final/fcbbioProject/myclone-output_combined/myclone_results_combined.tsv'
+maf_input_path = '/Users/biancakolim/Desktop/Academic/Spring2026/FCBB/final/data/raw'
+output_path = '/Users/biancakolim/Desktop/Academic/Spring2026/FCBB/final/fcbbioProject/myclone-output_combined/myclone_results_combined_genes.tsv'
+
+# map mutation ID to gene name
+def map_mutation_to_gene(maf_df):
+    maf_df["mutation_id"] = (
+        maf_df["Chromosome"].astype(str)
+        + ":"
+        + maf_df["Start_Position"].astype(str)
+        + ":"
+        + maf_df["Reference_Allele"]
+        + ">"
+        + maf_df["Tumor_Seq_Allele2"]
+    )
+    mutation_to_gene = maf_df[["mutation_id", "Hugo_Symbol"]]
+    return mutation_to_gene
+
+# get unique list of patients in myclone_results_combined
+myclone_results_combined_df = pd.read_csv(input_path, sep='\t')
+myclone_patients = list(set(myclone_results_combined_df["sample_id"]))
+
+# reformat sample sheet
+ss = pd.read_csv('gdc_sample_sheet.2026-04-01.tsv', sep='\t') #CHANGED
+ss = ss[['File ID', 'File Name', 'Data Type', 'Case ID']].copy()
+ss['Case ID'] = ss['Case ID'].str[:13] # only keep first 13 characters to match sample_id format in myclone results
+ss = ss.rename(columns={'Case ID': 'sample_id'}) # rename to match sample_id format in myclone results
+ss = ss[ss["sample_id"].isin(myclone_patients)] # only keep samples in myclone results
+ss_maf = ss.loc[ss['Data Type'] == 'Masked Somatic Mutation'].copy() # only keep MAF files
+
+gene_dfs = [] # list of dataframes to combine later
+for case in myclone_patients:
+    info = ss_maf.loc[ss_maf['sample_id'] == case].copy()
+    maf_path = os.path.join(maf_input_path, info['File ID'].iloc[0], info['File Name'].iloc[0])
+    try:
+        maf_df = pd.read_csv(maf_path, sep='\t', compression='gzip', comment='#')
+    except Exception as e:
+        print(f"Error reading MAF file {maf_path}: {e}, skipping sample {case}")
+        continue
+    
+    mutation_to_gene = map_mutation_to_gene(maf_df)
+
+    case_df = myclone_results_combined_df.loc[myclone_results_combined_df['sample_id'] == case].copy()
+
+    case_df = case_df.merge(mutation_to_gene, left_on='mutation_id', right_on='mutation_id', how='left')
+    gene_dfs.append(case_df)
+
+# Combine all gene dataframes
+myclone_results_combined_genes = pd.concat(gene_dfs, ignore_index=True)
+
+print(myclone_results_combined_genes.shape)
+print(myclone_results_combined_genes.head())
+
+myclone_results_combined_genes.to_csv(output_path, sep='\t', index=False)
